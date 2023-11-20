25826841d17f840b9673ecf70bda48e5e8eae4fa
jmohf <987976784@qq.com>
2023-11-21 06:35:03 +0800

    hot fix first

--- src/main/java/com/test/Commit.java
+++ src/main/java/com/test/Commit.java
@@ -7,6 +7,7 @@ package com.test;
 public class Commit {
     public static void main(String[] args) {
         System.out.println("this is test...");
+
         System.out.println("hot fix");
     }
 }
