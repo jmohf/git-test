0abeb49b3d8cf8ebbab72270fe6837643a81c17e
jmohf <987976784@qq.com>
2023-11-21 05:26:31 +0800

    first commit

---
+++ src/main/java/com/test/Commit.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author JMOHF
+ * @date 2023/11/21 5:23
+ */
+public class Commit {
+    public static void main(String[] args) {
+        System.out.println("this is test...");
+    }
+}
